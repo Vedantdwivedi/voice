5f404bb6a4127672368542e0a8b26a1bf5ab64d1
Vedant Dwivedi <Vedant.dwivedi55@gmail.com>
2024-07-14 09:20:57 +0530

    Update Nav.tsx

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -50,7 +50,7 @@ export const Nav = () => {
           <span>
             <Github className={"size-4"} />
           </span>
-          <span>Star on GitHub</span>
+          <span>Follow &ai</span>
         </Button>
         <Button
           onClick={toggleDark}
